97853165b8c9ea5937f63a7e26a25e6dfd0a7ea8
AseelBouziah <71261818+AseelBouziah@users.noreply.github.com>
2021-05-13 01:23:30 +0300

    Add files via upload

--- server/App.js
+++ server/App.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mysql = require('mysql');
+const { default: finalfinal } = require("./models/finalfinal.module.js");
+const { default: user } = require("./models/customer.module.js");
+
+//create database connection
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'gradproject'
+});
+//connect to database
+conn.connect((err) => {
+  if (err) throw err;
+  console.log('Mysql Connected...');
+});
+
 
 const App = express();
-
+App.use(bodyParser.json({ type: '/' }));
 // parse requests of content-type: application/json
 App.use(bodyParser.json());
 
@@ -14,6 +31,65 @@ App.get("/", (req, res) => {
   res.json({ message: "Welcome to oregano grad application." });
 });
 
+App.post('/logini/', function (req, res) {
+  email = req.body.email;
+  password = req.body.password;
+  conn.query("SELECT * FROM user WHERE  password= '" + password + "' and email= '" + email + "'", function (err, result, fields) {
+    if (err) {
+      response = {
+        result: "failed",
+      };
+      return;
+    }
+    if (result.length > 0) {
+      response = {
+        result: "in",
+      };
+    } else {
+      response = {
+        result: "success",
+      };
+    }
+    res.end(JSON.stringify(response));
+  });
+});
+
+App.get(/search\/(?:([^\/]+?))\/?$/i, function (req, res) {
+  var search = req.params[0]
+  var sql = "SELECT * from finalfinal WHERE list_title LIKE '%"+search+"%'"
+  conn.query(sql, function (err, result) {
+    if (err) res.send(err)
+    res.send(result)
+  })
+})
+
+
+App.get(/searcha\/(?:([^\/]+?))\/?$/i, function (req, res) {
+  var search = req.params[0]
+  var sql = "SELECT * from finalfinal WHERE list_Idiv LIKE '%"+search+"%'"
+  conn.query(sql, function (err, result) {
+    if (err) res.send(err)
+    res.send(result)
+  })
+})
+
+App.get(/searchb\/(?:([^\/]+?))\/?$/i, function (req, res) {
+  var search = req.params[0]
+  var sql = "SELECT * from finalfinal WHERE list_time1A LIKE '%"+search+"%'"
+  conn.query(sql, function (err, result) {
+    if (err) res.send(err)
+    res.send(result)
+  })
+})
+
+App.get(/searchc\/(?:([^\/]+?))\/?$/i, function (req, res) {
+  var search = req.params[0]
+  var sql = "SELECT * from finalfinal WHERE list_countA LIKE '%"+search+"%'"
+  conn.query(sql, function (err, result) {
+    if (err) res.send(err)
+    res.send(result)
+  })
+})
 
 require("./routes/names.routes.js")(App);
 
