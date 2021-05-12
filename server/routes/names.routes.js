49319f5e260a5ecb1474ecb7418152c36f45f5f2
AseelBouziah <71261818+AseelBouziah@users.noreply.github.com>
2021-05-13 01:24:24 +0300

    Add files via upload

--- server/routes/names.routes.js
+++ server/routes/names.routes.js
@@ -1,9 +1,34 @@
 module.exports = app => {
     const names = require("../controller/names.controller.js");
     const customers = require("../controller/customer.controller.js");
+    const imgtitle = require("../controller/imgtitle.controller.js");
+    const taabkh = require("../controller/finalfinal.controller.js");
+    const recipe = require ("../controller/recipe.controller.js");
+  
   
     
-  
+    app.get(/search\/(?:([^\/]+?))\/?$/i, function (req, res) {
+      var search = req.params[0]
+      var sql = "SELECT * from college_of_engh1 WHERE HNumber LIKE '%"+search+"%'"
+      conn.query(sql, function (err, result) {
+        if (err) res.send(err)
+        res.send(result)
+      })
+    })
+
+    
+   //إضافة وصفة
+
+   app.post("/recipe", recipe.create);
+
+
+   // إعادة وصفاتك 
+   app.get("/recipe", recipe.findAll);
+
+
+    //log in 
+    app.post("/login", customers.login); 
+
     // Retrieve all Customers
    app.get("/names", names.findAll);
    
@@ -12,6 +37,12 @@ module.exports = app => {
 
   // Retrieve all Customers
   app.get("/customers", customers.findAll);
+
+  // Retrieve all recipes
+  app.get("/finalfinal", taabkh.findAll);
+
+  // Retrieve all imgtitle
+  app.get("/imgtitle", imgtitle.findAll);
 
   // Retrieve a single Customer with customerId
   app.get("/customers/:customerId", customers.findOne);
